61d9d70b7cab502d04aad188e33ff48728c06725
Julius Franklin <jjfranklin88@outlook.com>
2021-11-04 14:00:35 -0400

    Order test passing

---
+++ __tests__/Order.test.js
@@ -0,0 +1,54 @@
+const pool = require('../lib/utils/pool');
+const twilio = require('twilio');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Order = require('../lib/models/Order');
+
+
+describe('get all', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  beforeEach(async() => {
+    return await Order.insert(3);
+  });
+
+  it('returns an array of orders', async() => {
+    const expected =
+      [
+        { id: expect.any(String), quantity: expect.any(Number) }
+      ];
+
+    expect(await Order.selectAll()).toEqual(expected);
+  });
+
+  it('returns an order by id', async() => {
+    const expected =
+
+        { id: expect.any(String), quantity: expect.any(Number) };
+
+
+    expect(await Order.selectById(1)).toEqual(expected);
+  });
+
+  it('updates an order', async() => {
+    const expected =
+
+        { id: expect.any(String), quantity: expect.any(Number) };
+
+
+    expect(await Order.update(1, 1)).toEqual(expected);
+  });
+
+  it('delets an order', async() => {
+    const expected =
+
+        { id: expect.any(String), quantity: expect.any(Number) };
+
+
+    expect(await Order.delete(1)).toEqual(expected);
+  });
+
+
+});
